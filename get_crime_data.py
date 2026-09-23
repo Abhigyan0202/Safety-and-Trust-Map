4cf30350a5f566d3cd15c9780ea4f1ced5b97252
Abhigyan0202 <bhatnagarabhigyan2004@gmail.com>
2026-09-23 17:52:41 +0000

    Added get_crime_data.py

---
+++ get_crime_data.py
@@ -0,0 +1,55 @@
+from playwright.sync_api import sync_playwright
+import time
+from subs import get_substations
+import pandas
+
+
+
+
+def get_results(df,browser,substation,start_date,end_date):
+    page = browser.new_page()
+    page.goto("https://citizen.mahapolice.gov.in/Citizen/MH/PublishedFIRs.aspx")
+    page.reload()
+    time.sleep(4)
+    page.locator("#ContentPlaceHolder1_ddlDistrict").select_option("BRIHAN MUMBAI CITY")
+    page.locator("#ContentPlaceHolder1_ddlPoliceStation").select_option(substation)
+    page.locator("#ContentPlaceHolder1_txtDateOfRegistrationFrom").press_sequentially(start_date)
+    page.locator("#ContentPlaceHolder1_txtDateOfRegistrationTo").press_sequentially(end_date)
+    page.click("#ContentPlaceHolder1_btnSearch")
+    time.sleep(3)
+    FIRs = page.locator("#ContentPlaceHolder1_lbltotalrecord").inner_text()
+    df.loc[len(df)] = [substation, start_date, end_date, FIRs]
+    page.close()
+            
+def run(df,substations, start_date, end_date):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        for s in substations:
+            get_results(df,browser,s,start_date,end_date)
+        browser.close()
+        
+       
+        
+
+if __name__ == "__main__":
+    substations = get_substations()
+    #supppose date is 01/08/2026, then we have to give it as input - 01082026
+    months = [["01082026","31082026"],
+              ["01072026","31072026"],
+              ["01062026","30062026"],
+              ["01052026","31052026"],
+              ["01042026","30042026"],
+              ["01032026","31032026"],
+              ["01022026","28022026"],
+              ["01012026","31012026"],
+              ["01122025","31122025"],
+              ["01112025","30112025"],
+              ["01102025","31102025"],
+              ["01092025","30092025"]]
+    for i in range(12):
+        print(months[i][0], months[i][1])
+    
+    # for i in range(12):
+    #     df = pandas.DataFrame(columns=["Name", "StartDate","EndDate", "FIRs"])
+    #     run(df, substations,months[i][0],months[i][1])
+    #     df.to_csv(f"df_{months[i][0]}.csv")
